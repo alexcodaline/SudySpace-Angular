ac656f59dd4b0462baf1e11e38830b204e50920c
alexcodaline <aleksander.ponom@gmail.com>
2023-10-23 18:15:09 +0300

    Lesson54 Pipe

---
+++ lesson54/src/app/pipes/filter.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+  transform(items: any[]): any[] {
+    if (!items) return [];
+
+    return items.slice().sort((a, b) => {
+      const nameA = a.name.toLowerCase();
+      const nameB = b.name.toLowerCase();
+
+      if (nameA < nameB) return -1;
+      if (nameA > nameB) return 1;
+      return 0;
+    });
+  }
+}
